3cf79764515ee8c32bcfe884cadc64b33eb90a1a
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-10-24 18:16:02 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -10,5 +10,13 @@ it
   do second_number = get_variable_from_file('./calculator,rb',"second_number")
   
   expect(second_number).not_to equal (0)
-  
-end
+end  
+
+it 
+  do
+    first_number = get_variable_from_file('./calculator.rb', "first_number")
+    second_number = get_variable_from_file('./calculator.rb', "second_number")
+    sum = get_variable_from_file('./calculator.rb', "sum")
+
+    expect(sum).to eq(first_number+second_number)
+  end
