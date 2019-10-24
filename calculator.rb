23fa4c49851e49549e06451d6be0ad05b142cdb6
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-10-24 18:07:54 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,13 +1,13 @@
 
-do
-first_number = get_variable_from_file('./calculator,rb',"first_number")
+it 
+do first_number = get_variable_from_file('./calculator,rb',"first_number")
 
 expect(first_number).to be_an(Integer).or be_a(Float)
 
 end
 
-do
-  second_number = get_variable_from_file('./calculator,rb',"second_number")
+it 
+  do second_number = get_variable_from_file('./calculator,rb',"second_number")
   
   expect(second_number).not_to equal (0)
   
