51719ab71a4ba3c9d2ae64d4b179bc520e6b3e65
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-10-24 18:08:25 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 
 it 
-do first_number = get_variable_from_file('./calculator,rb',"first_number")
+  do first_number = get_variable_from_file('./calculator,rb',"first_number")
 
 expect(first_number).to be_an(Integer).or be_a(Float)
 
